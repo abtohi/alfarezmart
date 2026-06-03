37e01bdb511bbbb26974235f02488ad514401b8b
alfarez <alfarezmart@gmail.com>
2026-06-03 18:47:43 +0700

    chore: bump version to 9.3 to invalidate cache for alignment fix

--- app/views/layouts/app.php
+++ app/views/layouts/app.php
@@ -49,7 +49,7 @@ if ($userLevel === 'staff') {
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     
     <!-- App CSS -->
-    <?php $v = '?v=9.2'; ?>
+    <?php $v = '?v=9.3'; ?>
     <link rel="stylesheet" href="<?= BASE_URL ?>public/css/variables.css<?= $v ?>">
     <link rel="stylesheet" href="<?= BASE_URL ?>public/css/app.css<?= $v ?>">
     <link rel="stylesheet" href="<?= BASE_URL ?>public/css/components.css<?= $v ?>">
