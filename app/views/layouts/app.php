0389c5b9c99ebc7addbbf2ef810cd0eb012eb091
alfarez <alfarezmart@gmail.com>
2026-06-15 20:23:46 +0700

    chore(version): bump version to 11.23

--- app/views/layouts/app.php
+++ app/views/layouts/app.php
@@ -237,7 +237,7 @@ if ($userLevel === 'staff') {
     <!-- App JS -->
     <script>
         const BASE_URL = '<?= BASE_URL ?>';
-        const version = '11.21';
+        const version = '11.23';
     </script>
     <script src="<?= BASE_URL ?>public/js/utils.js<?= $v ?>"></script>
     <script src="<?= BASE_URL ?>public/js/dexie.min.js<?= $v ?>"></script>
