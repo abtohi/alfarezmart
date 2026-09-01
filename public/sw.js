7d3961485f493ab6142f031dc53b583e314fcae1
alfarez <alfarezmart@gmail.com>
2026-09-01 09:36:00 +0700

    chore: bump app version to 25.12 and update service worker cache to force client update

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,8 @@
 /**
  * AlfarezMart PWA - Service Worker (public/sw.js)
  */
-const CACHE_NAME = 'alfarezmart-cache-v25.08';
-const DYNAMIC_CACHE = 'alfarezmart-dynamic-v25.08';
+const CACHE_NAME = 'alfarezmart-cache-v25.12';
+const DYNAMIC_CACHE = 'alfarezmart-dynamic-v25.12';
 const BASE_URL = self.location.pathname.replace('/public/sw.js', '/');
 const CORE_ASSETS = [
     BASE_URL,
